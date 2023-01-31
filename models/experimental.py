e96e1436dfea8a166ab1849eec936133fc28b295
zyc <645251181@qq.com>
2023-01-31 19:31:05 +0800

    2023-1-31-4

--- models/experimental.py
+++ models/experimental.py
@@ -1,111 +1,77 @@
-# YOLOv5 🚀 by Ultralytics, GPL-3.0 license
-"""
-Experimental modules
-"""
-import math
-
-import numpy as np
-import torch
-import torch.nn as nn
-
-from utils.downloads import attempt_download
-
+from models.common import *
 
 class Sum(nn.Module):
-    # Weighted sum of 2 or more layers https://arxiv.org/abs/1911.09070
-    def __init__(self, n, weight=False):  # n: number of inputs
-        super().__init__()
-        self.weight = weight  # apply weights boolean
-        self.iter = range(n - 1)  # iter object
+    def __init__(self, n, weight=False):
+        super(Sum, self).__init__()
+        self.weight = weight
+        self.iter = range(n - 1)
         if weight:
-            self.w = nn.Parameter(-torch.arange(1.0, n) / 2, requires_grad=True)  # layer weights
+            self.w = nn.Parameter(-torch.arange(1., n) / 2, requires_grad=True)
 
     def forward(self, x):
-        y = x[0]  # no weight
+        y = x[0]
         if self.weight:
             w = torch.sigmoid(self.w) * 2
             for i in self.iter:
-                y = y + x[i + 1] * w[i]
+                y = y + x[i+1]
         else:
             for i in self.iter:
                 y = y + x[i + 1]
         return y
 
 
+class GhostConv(nn.Module):
+    def __init__(self, c1, c2, k=1, s=1, g=1, act=True):
+        super(GhostConv, self).__init__()
+        c_ = c2 // 2
+        self.cv1 = Conv(c1, c_, k, s, g, act)
+        self.cv2 = Conv(c_, c_, 5, 1, c_, act)
+
+    def forward(self, x):
+        y = self.cv1(x)
+        return torch.cat([y, self.cv2(y)], 1)
+
+class GhostBottleneck(nn.Module):
+    def __init__(self, c1, c2, k, s):
+        super(GhostBottleneck, self).__init__()
+        c_ = c2 // 2
+        self.conv = nn.Sequential(GhostConv(c1, c_, 1, 1),
+                                DWConv(c_, c_, k, s, act=False) if s == 2 else nn.Identity(),
+                                GhostConv(c_, c2, 1, 1, act=False))
+        self.shortcut = nn.Sequential(DWConv(c1, c1, k, s, act=False),
+                        Conv(c1, c2, 1, 1, act=False)) if s == 2 else nn.Identity()
+
+    def forward(self, x):
+        return self.conv(x) + self.shortcut(x)
+
+class ConvPlus(nn.Module):
+    def __init__(self, c1, c2, k=3, s=1, g=1, bias=True):
+        super(ConvPlus, self).__init__()
+        self.cv1 = nn.Conv2d(c1, c2, (k, 1), s, (k // 2, 0), groups=g, bias=bias)
+        self.cv2 = nn.Conv2d(c1, c2, (1, k), s, (0, k // 2), groups=g, bias=bias)
+
+    def forward(self, x):
+        return self.cv1(x) + self.cv2(x)
+
+
 class MixConv2d(nn.Module):
-    # Mixed Depth-wise Conv https://arxiv.org/abs/1907.09595
-    def __init__(self, c1, c2, k=(1, 3), s=1, equal_ch=True):  # ch_in, ch_out, kernel, stride, ch_strategy
-        super().__init__()
-        n = len(k)  # number of convolutions
-        if equal_ch:  # equal c_ per group
-            i = torch.linspace(0, n - 1E-6, c2).floor()  # c2 indices
-            c_ = [(i == g).sum() for g in range(n)]  # intermediate channels
-        else:  # equal weight.numel() per group
-            b = [c2] + [0] * n
-            a = np.eye(n + 1, n, k=-1)
+    def __init__(self, c1, c2, k=(1, 3), s=1, equal_ch=True):
+        super(MixConv2d, self).__init__()
+        groups = len(k)
+        if equal_ch:
+            i = torch.linspace(0, groups - 1E-6, c2).floor()
+            c_ = [(i == g).sum() for g in range(groups)]
+        else:
+            b = [c2] + [0] * groups
+            a = np.eye(groups + 1, groups, k=-1)
             a -= np.roll(a, 1, axis=1)
             a *= np.array(k) ** 2
             a[0] = 1
-            c_ = np.linalg.lstsq(a, b, rcond=None)[0].round()  # solve for equal weight indices, ax = b
+            c_ = np.linalg.lstsq(a, b, rcond=None)[0].round()
 
-        self.m = nn.ModuleList([
-            nn.Conv2d(c1, int(c_), k, s, k // 2, groups=math.gcd(c1, int(c_)), bias=False) for k, c_ in zip(k, c_)])
+        self.m = nn.ModuleList([nn.Conv2d(c1, int(c_[g]), k[g], s, k[g] // 2, bias=False) for g in range(groups)])
         self.bn = nn.BatchNorm2d(c2)
-        self.act = nn.SiLU()
+        self.act = nn.LeakyReLU(0.1, inplace=True)
 
     def forward(self, x):
-        return self.act(self.bn(torch.cat([m(x) for m in self.m], 1)))
-
-
-class Ensemble(nn.ModuleList):
-    # Ensemble of models
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, x, augment=False, profile=False, visualize=False):
-        y = [module(x, augment, profile, visualize)[0] for module in self]
-        # y = torch.stack(y).max(0)[0]  # max ensemble
-        # y = torch.stack(y).mean(0)  # mean ensemble
-        y = torch.cat(y, 1)  # nms ensemble
-        return y, None  # inference, train output
-
-
-def attempt_load(weights, device=None, inplace=True, fuse=True):
-    # Loads an ensemble of models weights=[a,b,c] or a single model weights=[a] or weights=a
-    from models.yolo import Detect, Model
-
-    model = Ensemble()
-    for w in weights if isinstance(weights, list) else [weights]:
-        ckpt = torch.load(attempt_download(w), map_location='cpu')  # load
-        ckpt = (ckpt.get('ema') or ckpt['model']).to(device).float()  # FP32 model
-
-        # Model compatibility updates
-        if not hasattr(ckpt, 'stride'):
-            ckpt.stride = torch.tensor([32.])
-        if hasattr(ckpt, 'names') and isinstance(ckpt.names, (list, tuple)):
-            ckpt.names = dict(enumerate(ckpt.names))  # convert to dict
-
-        model.append(ckpt.fuse().eval() if fuse and hasattr(ckpt, 'fuse') else ckpt.eval())  # model in eval mode
-
-    # Module compatibility updates
-    for m in model.modules():
-        t = type(m)
-        if t in (nn.Hardswish, nn.LeakyReLU, nn.ReLU, nn.ReLU6, nn.SiLU, Detect, Model):
-            m.inplace = inplace  # torch 1.7.0 compatibility
-            if t is Detect and not isinstance(m.anchor_grid, list):
-                delattr(m, 'anchor_grid')
-                setattr(m, 'anchor_grid', [torch.zeros(1)] * m.nl)
-        elif t is nn.Upsample and not hasattr(m, 'recompute_scale_factor'):
-            m.recompute_scale_factor = None  # torch 1.11.0 compatibility
-
-    # Return model
-    if len(model) == 1:
-        return model[-1]
-
-    # Return detection ensemble
-    print(f'Ensemble created with {weights}\n')
-    for k in 'names', 'nc', 'yaml':
-        setattr(model, k, getattr(model[0], k))
-    model.stride = model[torch.argmax(torch.tensor([m.stride.max() for m in model])).int()].stride  # max stride
-    assert all(model[0].nc == m.nc for m in model), f'Models have different class counts: {[m.nc for m in model]}'
-    return model
+        return x + self.act(self.bn(torch.cat([m(x) for m in self.m], 1)))
--- models/yolo.py
+++ models/yolo.py
@@ -2,44 +2,44 @@ import argparse
 
 from models.experimental import *
 
+
 class Detect(nn.Module):
-    def __init__(self, nc=15, anchors=()): #检测层
+    def __init__(self, nc=80, anchors=()):  # detection layer
         super(Detect, self).__init__()
-        self.stride = None # 建立模型时计算步幅
-        self.nc = nc # 分类的个数
-        self.no = nc + 5 # 每一个锚框输出元素的个数
-        self.nl = len(anchors) # 检测层的数量
-        self.na = len(anchors[0]) // 2 # 锚框的个数
-        self.grid = [torch.zeros(1)] * self.nl # 初始化网格
+        self.stride = None  # strides computed during build
+        self.nc = nc  # number of classes
+        self.no = nc + 5  # number of outputs per anchor
+        self.nl = len(anchors)  # number of detection layers
+        self.na = len(anchors[0]) // 2  # number of anchors
+        self.grid = [torch.zeros(1)] * self.nl  # init grid
         a = torch.tensor(anchors).float().view(self.nl, -1, 2)
-        self.register_buffer('anchors', a) # shape(nl, na, 2)
-        self.register_buffer('anchor_grid', a.clone().view(self.nl, 1, -1, 1, 1, 2)) # shape(nl, 1, na, 1, 1, 2)
-        self.export = False
+        self.register_buffer('anchors', a)  # shape(nl,na,2)
+        self.register_buffer('anchor_grid', a.clone().view(self.nl, 1, -1, 1, 1, 2))  # shape(nl,1,na,1,1,2)
+        self.export = False  # onnx export
 
     def forward(self, x):
-        z = [] # 推理输出
+        # x = x.copy()  # for profiling
+        z = []  # inference output
         self.training |= self.export
         for i in range(self.nl):
-            bs, _, ny, nx = x[i].shape # x(bs, 255, 20, 20) to x(bs, 3, 20, 20, 85)
+            bs, _, ny, nx = x[i].shape  # x(bs,255,20,20) to x(bs,3,20,20,85)
             x[i] = x[i].view(bs, self.na, self.no, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
 
-            if not self.training:
+            if not self.training:  # inference
                 if self.grid[i].shape[2:4] != x[i].shape[2:4]:
                     self.grid[i] = self._make_grid(nx, ny).to(x[i].device)
 
-                    y = x[i].sigmoid()
-                    y[..., 0:2] = (y[..., 0:2] * 2. - 0.5 + self.grid[i].to(x[i].device)) * self.stride[i]
-                    y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]
-                    z.append(y.view(bs, -1, self.no))
+                y = x[i].sigmoid()
+                y[..., 0:2] = (y[..., 0:2] * 2. - 0.5 + self.grid[i].to(x[i].device)) * self.stride[i]  # xy
+                y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
+                z.append(y.view(bs, -1, self.no))
 
         return x if self.training else (torch.cat(z, 1), x)
-
 
     @staticmethod
     def _make_grid(nx=20, ny=20):
         yv, xv = torch.meshgrid([torch.arange(ny), torch.arange(nx)])
         return torch.stack((xv, yv), 2).view((1, 1, ny, nx, 2)).float()
-
 
 class Model(nn.Module):
     def __init__(self, model_cfg='yolov5l.yaml', ch=3, nc=15):
@@ -49,4 +49,145 @@ class Model(nn.Module):
         else:
             with open(model_cfg) as f:
                 self.md = yaml.load(f, Loader=yaml.FullLoader)
-                
+
+        if nc:
+            self.md['nc'] = nc # overide yaml value
+        self.model, self.save = parse_model(self.md, ch=[ch]) # model, savelist, ch_out
+
+        #建立步幅，锚
+        m = self.model[-1]
+        m.stride = torch.tensor([128 / x.shape[-2] for x in self.forward(torch.zeros(1, ch, 128, 128))])
+        m.anchors /= m.stride.view(-1, 1, 1)
+        check_anchor_order(m)
+        self.stride = m.stride
+
+        #初始化权重，偏置
+        torch_utils.initialize_weights(self)
+        self._initialize_biases()
+        torch_utils.model_info(self)
+        print('')
+
+    def forward(self, x, augment=False, profile=False):
+        if augment:
+            img_size = x.shape[-2:] # 高、宽
+            s = [0.83, 0.67]
+            y = []
+            for i, xi in enumerate((x, 
+                                torch_utils.scale_img(x.flip(3), s[0]),
+                                torch_utils.scale_img(x, s[1]),
+                                )): 
+                y.append(self.forward_once(xi)[0])
+            y[1][..., :4] /= s[0]
+            y[2][..., 0] = img_size[1] - y[1][..., 0]
+            y[2][..., :4] /= s[1]
+            return torch.cat(y, 1), None
+        else:
+            return self.forward_once(x, profile)
+
+    def forward_once(self, x, profile=False):
+        y, dt = [], []
+        for m in self.model:
+            if m.f != -1:
+                x = y[m.f] if isinstance(m.f, int) else [x if j == -1 else y[j] for j in m.f]
+
+            if profile:
+                try:
+                    import thop
+                    o = thop.profile(m, inputs=(x,), verbose=False)[0] / 1E9 * 2
+                except:
+                    o = 0
+                t = torch_utils.time_synchronized()
+                for _ in range(10):
+                    _ = m(x)
+                dt.append((torch_utils.time_synchronized() - t) * 100)
+                print('%10.1f%10.0f%10.1fms %-40s' % (o, m.np, dt[-1], m.type))
+
+            x = m(x)
+            y.append(x if m.i in self.save else None)
+        
+        if profile:
+            print('%.1fms total' % sum(dt))
+        return x
+
+    def _initialize_biases(self, cf=None):  # initialize biases into Detect(), cf is class frequency
+        # cf = torch.bincount(torch.tensor(np.concatenate(dataset.labels, 0)[:, 0]).long(), minlength=nc) + 1.
+        m = self.model[-1]  # Detect() module
+        for f, s in zip(m.f, m.stride):  #  from
+            mi = self.model[f % m.i]
+            b = mi.bias.view(m.na, -1)  # conv.bias(255) to (3,85)
+            b[:, 4] += math.log(8 / (640 / s) ** 2)  # obj (8 objects per 640 image)
+            b[:, 5:] += math.log(0.6 / (m.nc - 0.99)) if cf is None else torch.log(cf / cf.sum())  # cls
+            mi.bias = torch.nn.Parameter(b.view(-1), requires_grad=True)
+
+    def _print_biases(self):
+        m = self.model[-1] # Detect() module
+        for f in sorted([x % m.i for x in m.f]):
+            b = self.model[f].bias.detach().view(m.na, -1).T
+            print(('%g Conv2d.bias:' + '%10.3g' * 6) % (f, *b[:5].mean(1).tolist(), b[5:].mean()))
+
+    def fuse(self):
+        print('Fusing layers...')
+        for m in self.model.modules():
+            if type(m) is Conv:
+                m.conv = torch_utils.fuse_conv_and_bn(m.conv, m.bn)
+                m.bn = None
+                m.forward = m.fuseforward
+        torch_utils.model_info(self)
+
+
+def parse_model(md, ch): # 模型字典， 输入通道
+    print('\n%3s%15s%3s%10s %-40s%-30s' % ('', 'from', 'n', 'params', 'modules', 'arguments'))
+    anchors, nc, gd, gw = md['anchors'], md['nc'], md['depth_multiple'], md['width_multiple']
+    na = (len(anchors[0]) // 2) # number of anchors
+    no = na * (nc + 5) # number of output = anchors * (classes + 5)
+
+    layers, save, c2 = [], [], ch[-1]
+    for i, (f, n, m, args) in enumerate(md['backbone'] + md['head']):
+        m = eval(m) if isinstance(m, str) else m # eval strings
+        for j, a in enumerate(args):
+            try:
+                args[j] = eval(a) if isinstance(a, str) else a
+            except:
+                pass
+        
+        n = max(round(n * gd), 1) if n > 1 else n # depth gain
+        if m in [nn.Conv2d, Conv, Bottleneck, SPP, DWConv, MixConv2d, Focus, ConvPlus, BottleneckCSP]:
+            c1, c2 = ch[f], args[0]
+
+            c2 = make_divisible(c2 * gw, 8) if c2 != no else c2
+
+            args = [c1, c2, *args[1:]]
+            if m is BottleneckCSP:
+                args.insert(2, n)
+                n = 1
+
+        elif m is nn.BatchNorm2d:
+            args = [ch[f]]
+        elif m is Concat:
+            c2 = sum([ch[-1 if x == -1 else x + 1] for x in f])
+        elif m is Detect:
+            f = f or list(reversed([(-1 if j == i else j - 1) for j, x in enumerate(ch) if x == no]))
+        else:
+            c2 = ch[f]
+
+        m_ = nn.Sequential(*[m(*args) for _ in range(n)]) if n > 1 else m(*args)
+        t = str(m)[8:-2].replace('__main__.', '')
+        np = sum([x.numel() for x in m_.parameters()])
+        m_.i, m_.f, m_.type, m_.np = i, f, t, np # attach index, 'from' index, type, number params
+        print('%3s%15s%3s%10.0f %-40s%-30s' % (i, f, n, np, t, args))
+        save.extend(x % i for x in ([f] if isinstance(f, int) else f) if x != -1)
+        layers.append(m_)
+        ch.append(c2)
+    return nn.Sequential(*layers), sorted(save)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cfg', type=str, default='yolov5l.yaml', help='model.yaml')
+    parser.add_argument('--device', default='', help='cuda device, i.e 0 or 0,1,2,3 or cpu')
+    opt = parser.parse_args()
+    opt.cfg = check_file(opt.cfg)
+    device = torch_utils.select_device(opt.device)
+
+    # 创建模型
+    model = Model(opt.cfg).to(device)
+    model.train()
